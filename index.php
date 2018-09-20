916f07fedc5970351ebf8f4c35b1cedc3026df6b
ericyork <eyork@clarkson.edu>
2018-09-20 10:56:12 -0400

    Adds main-nav.php for site navigation

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
-<?php
-  include 'inc/head.php';
-?>
-<nav class="main-nav">
-</nav>
-    <h1>Hello, world!</h1>
+<?php include 'inc/head.php';?>
+<?php include 'inc/main-nav.php';?>
+<h1>Hello, world!</h1>
 
     <!-- Bootstrap JS Dependencies (first jQuery, then Popper.js, then Bootstrap) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
