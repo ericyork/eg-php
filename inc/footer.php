7b7add4f9e358f614f0ef1b48091f84db956d337
ericyork <eyork@clarkson.edu>
2018-09-20 11:57:05 -0400

    Adds logo, nav, footer, and head includes

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<footer class="fixed-bottom container-fluid border-top border-bottom">
+    <p style="text-align: center; margin-top: 1em;">&copy; 2018 Comm429</p>
+</footer>
+<!-- Bootstrap JS Dependencies (first jQuery, then Popper.js, then Bootstrap) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
